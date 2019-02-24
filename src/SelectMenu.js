8c43175e2ec365720669577ff219d793cb7e430c
glaze <i@inevitable.tech>
2019-02-23 20:15:02 -0800

    selectMenu.js:optimize click event

--- src/SelectMenu.js
+++ src/SelectMenu.js
@@ -1,40 +1,53 @@
-var pSelectText = "";
-console.log("Before mouse up", pSelectText);
-
+// var pSelectText = "";
+const $ = require("jquery");
 $(function() {
   var menu = $("#highlight_menu");
-  $(document.body).on("mouseup", function(evt) {
+  $(document.body).on("click", async function(evt) {
+    //wait the selection to refresh
+    await sleep(2);
     var s = document.getSelection();
-    // console.log(s.anchorNode)
-    r = s.getRangeAt(0);
-    if (r && s.toString() && s.toString() != pSelectText) {
-      pSelectText = s.toString();
-      console.log("after mouse up", pSelectText);
-      var p = r.getBoundingClientRect();
-      console.log(p);
-      if (p.left || p.top) {
-        menu
-          .css({
-            left: p.left + p.width / 2 - menu.width() / 2,
-            top: p.top - menu.height() - 10,
-            display: "block",
-            opacity: 0
-          })
-          .animate(
-            {
-              opacity: 1
-            },
-            300
-          );
-
-        setTimeout(function() {
-          menu.addClass("highlight_menu_animate");
-        }, 10);
-        return;
+    if (s.toString()) {
+      const r = s.getRangeAt(0);
+      if (r&&s.toString().trim()) {
+        console.log(s.toString())
+        // pSelectText = s.toString();
+        var p = r.getBoundingClientRect();
+        console.debug(p);
+        if (p.left || p.top) {
+          menu
+            .css({
+              left: p.left + p.width / 2 - menu.width() / 2,
+              top: p.top - menu.height() - 10,
+              display: "block",
+              //start with fully transparent
+              opacity: 0
+            })
+            .animate(
+              {
+                //becoming opaque
+                opacity: 0.9
+              },
+              300
+            );
+          // menu.addClass("highlight_menu_animate");
+          setTimeout(function() {
+            menu.addClass("highlight_menu_animate");
+          }, 1000);
+          return;
+        }
       }
     }
-    menu.animate({ opacity: 0 }, function() {
-      menu.hide().removeClass("highlight_menu_animate");
-    });
+    hide();
   });
 });
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function hide() {
+  var menu = $("#highlight_menu");
+  menu.animate({ opacity: 0 }, function() {
+    menu.hide().removeClass("highlight_menu_animate");
+  });
+}
